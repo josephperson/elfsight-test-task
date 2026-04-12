c38212da65aebda14a3f39844218e3c2caca9e22
JosephPerson <thejosephperson@mail.ru>
2026-04-12 21:36:10 +0300

    refactor(entry-point): improve root initialization with named imports and null check

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 import { DataProvider } from './components';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (!rootElement) throw new Error('root element not found!');
+
+const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <DataProvider>
       <App />
     </DataProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
